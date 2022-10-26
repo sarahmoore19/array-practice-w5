738530599546105973cc29458e932b301f38aa11
sarahmoore <sarahnmoore19@gmail.com>
2022-10-26 02:07:26 -0400

    solved, calculated time/space complexity

--- array-practice.js
+++ array-practice.js
@@ -1,39 +1,84 @@
 const findMinimum = arr => {
-
-  // Your code here
-
+if (arr.length === 0) return undefined;
+let high = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < high) high = arr[i];
+  }
+  return high;
 };
+// time: O(n) space: O(1)
 
 const runningSum = arr => {
-
-  // Your code here
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum+= arr[i];
+    arr[i] = sum;
+  }
+  return arr;
 };
+// time: O(n) space: O(1)
 
 const evenNumOfChars = arr => {
-
-  // Your code here
+  let count = 0;
+  arr.forEach((ele) => {
+    if (ele.length % 2 === 0) count++;
+  })
+  return count;
 };
+// time: O(n) space: O(1)
 
 const smallerThanCurr = arr => {
-
-  // Your code here
-
+  let newArr = [];
+  let count;
+  for (let i = 0; i < arr.length; i++) {
+    count = 0;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < arr[i]) count++;
+    }
+    newArr.push(count);
+  }
+  return newArr;
 };
+
+// time: O(n * n) space: O(n)
 
 const twoSum = (arr, target) => {
+  let newObj = {};
+  for (let i = 0; i < arr.length; i++) {
+  if ((target - arr[i]) in newObj) return true;
+  else newObj[arr[i]] = arr[i];
+  }
+  return false;
+};
+// time: O(n) space: O(n)
 
-  // Your code here
+let secondLargest = (arr, n = 2) => {
+  if (arr.length < n) return undefined;
+  let idx;
+  let high;
+  for (let i = n; i > 0; i--) {
+    high = -Infinity
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > high) {
+        high = arr[j];
+        idx = j;
+      }
+    }
+    if (i === 1) return high;
+
+    [arr[idx], arr[arr.length - 1]] = [arr[arr.length - 1], arr[idx]];
+    arr.pop();
+  }
 };
 
-const secondLargest = arr => {
-
-  // Your code here
-};
+// made into a dynamic function, will find nth highest number
+// time: O(n) space: O(1)
 
 const shuffle = (arr) => {
-
-  // Your code here
+  array = [...arr]
+  return array.sort(() => Math.random() - 0.5);
 };
 
+// time: O(n) space: O(n)
 
 module.exports = [findMinimum, runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
